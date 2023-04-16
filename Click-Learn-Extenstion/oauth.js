59518dd235e11030217c4c5116fca82c141290f9
harelsarag7 <112413505+harelsarag7@users.noreply.github.com>
2023-04-16 19:42:44 +0300

    fixed auth, and changed icon

--- Click-Learn-Extenstion/oauth.js
+++ Click-Learn-Extenstion/oauth.js
@@ -1,168 +1,78 @@
-  window.onload = function() {
-    chrome.storage.local.get('accessToken', function(result) {
-      const token = result.accessToken;
-      console.log('Token retrieved from storage:', token);
+window.onload = function() {
+  chrome.storage.local.get('accessToken', function(result) {
+    const token = result.accessToken;
+    console.log('Token retrieved from storage:', token);
 
-    if(result.accessToken){
-      chrome.identity.getAuthToken({interactive: true,scopes: ['profile']}, function(accessToken) {
-        let init = {
-              method: 'GET',
-              async: true,
-              headers: {
-                Authorization: 'Bearer ' + token,
-                'Content-Type': 'application/json'
-              },
-              'contentType': 'json'
-            };
-            fetch(
-              'https://people.googleapis.com/v1/people/me?personFields=names&key=AIzaSyAeyJVqlcotDtxTynhsN9p6HO0_V-gzEts',
-              init)
-              .then((response) => response.json())
-              .then(function(data) {
-                document.getElementsByClassName("loader")[0].style.display = "none";
-                document.getElementById("welcome_name").innerHTML = "Hello"+ " " + data.names[0].givenName + " " + data.names[0].familyName
-                document.getElementById("loginGoogle").style.display = "none";
-                document.getElementById("logoutGoogle").style.display = "block";
-              });
-          });
+    if (result.accessToken) {
+      getUserInfo(token);
     }
-
   });
 
-    document.querySelector('#loginGoogle').addEventListener('click', function(token) {
-        document.getElementsByClassName("loader")[0].style.display = "block";
-        getToken().then(() => {
-          document.getElementsByClassName("loader")[0].style.display = "none";
-          document.getElementById("welcome_name").innerHTML = "Hello"+ " " + data.names[0].givenName + " " + data.names[0].familyName
-          document.getElementById("loginGoogle").style.display = "none";
-          document.getElementById("logoutGoogle").style.display = "block";
-        });
+  document.querySelector('#loginGoogle').addEventListener('click', function(token) {
+    document.getElementsByClassName("loader")[0].style.display = "block";
 
-        });
+    chrome.identity.getAuthToken({interactive: true, scopes: ['profile']}, function(token) {
+      chrome.storage.local.set({ 'accessToken': token }, function() {
+        console.log("Value is set to " + token);
+      });
 
-        function getToken(){
-          chrome.identity.getAuthToken({interactive: true,scopes: ['profile']}, function(token) {
-            let init = {
-                  method: 'GET',
-                  async: true,
-                  headers: {
-                    Authorization: 'Bearer ' + token,
-                    'Content-Type': 'application/json'
-                  },
-                  'contentType': 'json'
-                };
-                fetch(
-                  'https://people.googleapis.com/v1/people/me?personFields=names&key=AIzaSyAeyJVqlcotDtxTynhsN9p6HO0_V-gzEts',
-                  init)
-                  .then((response) => response.json())
-                  .then(function(data) {
-                    console.log(token);
-              
-                    chrome.storage.local.set({ 'accessToken': token }).then(() => {
-                      console.log("Value is set to " + token);
-                    });
-                    console.log(data);
-                    return data
-             
-                  });
-              });
-        }
-  
-  
-  
-  
-        document.getElementById("logoutGoogle").addEventListener('click', function() {
-        document.getElementsByClassName("loader")[0].style.display = "block";
-        chrome.identity.getAuthToken({interactive: true}, function(token) {
-          console.log(token);
-          if (token) {
-            chrome.identity.removeCachedAuthToken({token: token}, function() {
-              console.log('Token removed.');
-              document.getElementsByClassName("loader")[0].style.display = "none";
-                  document.getElementById("loginGoogle").style.display = "flex";
-                  document.getElementById("logoutGoogle").style.display = "none";
-                  document.getElementById("welcome_name").innerHTML = "";
-                });
-              }
-            });
+      getUserInfo(token);
+    });
+  });
+
+  document.getElementById("logoutGoogle").addEventListener('click', function() {
+    document.getElementsByClassName("loader")[0].style.display = "block";
+    chrome.storage.local.get('accessToken', function(result) {
+      const token = result.accessToken;
+      if (token) {
+        revokeToken(token);
+        chrome.identity.removeCachedAuthToken({token: token}, function() {
+          console.log('Token removed.');
+          chrome.storage.local.remove('accessToken', function() {
+            console.log('Access token removed from storage.');
+            document.getElementsByClassName("loader")[0].style.display = "none";
+            document.getElementById("loginGoogle").style.display = "flex";
+            document.getElementById("logoutGoogle").style.display = "none";
+            document.getElementById("welcome_name").innerHTML = "";
           });
-  
-    };
+        });
+      }
+    });
+  });
 
-    function showProgressBar() {
-      let loaderDiv = document.getElementById("loader");
-      loaderDiv.innerHTML = '<progress id="progressBar" value="0" max="100"></progress>';
+};
+
+function getUserInfo(token) {
+  let init = {
+    method: 'GET',
+    async: true,
+    headers: {
+      Authorization: 'Bearer ' + token,
+      'Content-Type': 'application/json'
+    },
+    'contentType': 'json'
+  };
+  fetch(
+    'https://people.googleapis.com/v1/people/me?personFields=names&key=AIzaSyAeyJVqlcotDtxTynhsN9p6HO0_V-gzEts',
+    init)
+    .then((response) => response.json())
+    .then(function(data) {
+      document.getElementsByClassName("loader")[0].style.display = "none";
+      document.getElementById("welcome_name").innerHTML = "Hello" + " " + data.names[0].givenName + " " + data.names[0].familyName;
+      document.getElementById("loginGoogle").style.display = "none";
+      document.getElementById("logoutGoogle").style.display = "block";
+    });
+}
+
+function revokeToken(token) {
+  fetch('https://accounts.google.com/o/oauth2/revoke?token=' + token, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
     }
-    
-
-      
-        
-    // document.querySelector('button').addEventListener('click', function() {
-    //     document.getElementsByClassName("loader")[0].style.display = "block";
-    //     document.getElementById("loginGoogle").disabled = true;
-    //     setTimeout(() => {
-    //         document.getElementsByClassName("loader")[0].style.display = "none";
-    //     }, 2000)
-    //     chrome.identity.getAuthToken({interactive: true}, function(token) {
-    //         let init = {
-    //             method: 'GET',
-    //             async: true,
-    //             headers: {
-    //                 Authorization: 'Bearer ' + token,
-    //                 'Content-Type': 'application/json'
-    //             },
-    //             'contentType': 'json'
-    //         };
-    //         fetch(
-    //             'https://people.googleapis.com/v1/contactGroups/all?maxMembers=20&key=AIzaSyAeyJVqlcotDtxTynhsN9p6HO0_V-gzEts',
-    //             init)
-    //             .then((response) => response.json())
-    //             .then(function(data) {
-    //                 console.log(data)
-    //             });
-    //             document.getElementById("loginGoogle").disabled = false;
-    //         });
-    //     });
-
-
-  //   function revokeToken() {
-  //     user_info_div.innerHTML = "";
-  //     chrome.identity.getAuthToken({ interactive: false }, 
-  //       function (current_token) {
-  //         if (!chrome.runtime.lastError) {
-  //           // @corecode_begin removeAndRevokeAuthToken
-  //           // @corecode_begin removeCachedAuthToken
-  //           // Remove the local cached token
-  //           chrome.identity.removeCachedAuthToken({token: current_token}, function(){});
-  //           // @corecode_end removeCachedAuthToken
-  
-  //           // Make a request to revoke token in the server
-  //           var xhr = new XMLHttpRequest();
-  //           xhr.open(
-  //           "GET", 
-  //           "https://accounts.google.com/o/oauth2/revoke?token=" + current_token);
-              
-  //           xhr.send();
-  //           // @corecode_end removeAndRevokeAuthToken
-  
-  //           // Update the user interface accordingly
-  //           changeState(STATE_START);
-  //           sampleSupport.log("Token revoked and removed from cache. " + 
-  //             "Check chrome://identity-internals to confirm.");
-  //         }
-  //     });
-  // }
-//   window.onload = function() {
-  
-//     // Logout button event listener
-//     document.getElementById("logoutGoogle").addEventListener('click', function() {
-//       chrome.identity.getAuthToken({interactive: false}, function(token) {
-//         if (token) {
-//           chrome.identity.removeCachedAuthToken({token: token}, function() {
-//             console.log('Token removed.');
-//           });
-//         }
-//       });
-//     });
-//   };
-  
+  }).then(response => {
+    console.log('Token revoked:', response);
+  }).catch(error => {
+    console.error('Error revoking token:', error);
+  });
+}
--- Click-Learn-Extenstion/popup.js
+++ Click-Learn-Extenstion/popup.js
@@ -11,7 +11,7 @@ document.addEventListener('selectionchange', () => {
   if (selectedText && !isImageAppended) {
     icon = document.createElement("img");
     icon.id = "icon";
-    icon.src = "https://i.ibb.co/QXWwJ9h/Click-Learn-Icon.png";
+    icon.src = "https://i.ibb.co/9t8DNZX/image-2.png";
     icon.width = 30;
     icon.style.cursor = "pointer";
     const range = window.getSelection().getRangeAt(0);
@@ -100,11 +100,18 @@ function showModal(text) {
 
 
 
+  if(isSingleWord(text)){
 
+    
+    const EnglishSelectedText = document.createElement("p");
+    EnglishSelectedText.textContent = text;
+    ModalENContainer.appendChild(EnglishSelectedText);
+  } else {
+    const EnglishSelectedText = document.createElement("p");
+    EnglishSelectedText.textContent = "נא לסמן מילה אחת";
+    ModalENContainer.appendChild(EnglishSelectedText);
 
-  const EnglishSelectedText = document.createElement("p");
-  EnglishSelectedText.textContent = text;
-  ModalENContainer.appendChild(EnglishSelectedText);
+  }
 
   const HebrewTransltedText = document.createElement("p");
   HebrewTransltedText.textContent = text;
@@ -140,46 +147,6 @@ function showModal(text) {
 }
 
 
-// openAuth(){
-//   document.body.append()
-// }
-// openAuth() {
-//   document.body.innerHTML("<a href="https://accounts.google.com/o/oauth2/auth?state=%7B%22csrf%22%3A%22fed92707-6b3c-441a-96c4-32a098488080%22%2C%22success_page%22%3Anull%2C%22pkce_oauth%22%3Anull%7D&amp;scope=email+profile&amp;client_id=600979030768-7o35cjq1gv138e22oj0j39u0bp0mn3hj.apps.googleusercontent.com&amp;redirect_uri=https%3A%2F%2Ftodoist.com%2FUsers%2FgplusRedirect&amp;response_type=token&amp;apppackagename=com.todoist&amp;prompt=select_account" data-gtm-id="google-provider-link" aria-disabled="false" class="nFxHGeI SNaOf6C W-FS4gj _8313bd46 f169a390 _95951888 _2a3b75a1 _56a651f6"><div aria-hidden="true" class="a44d4350 _2a3b75a1 _509a57b4 e5a9206f"><svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" class="MfSWZ05" aria-hidden="true"><g fill="none" fill-rule="evenodd"><path d="M17.64 9.205c0-.639-.057-1.252-.164-1.841H9v3.481h4.844a4.14 4.14 0 01-1.796 2.716v2.259h2.908c1.702-1.567 2.684-3.875 2.684-6.615z" fill="#4285F4"></path><path d="M9 18c2.43 0 4.467-.806 5.956-2.18l-2.908-2.259c-.806.54-1.837.86-3.048.86-2.344 0-4.328-1.584-5.036-3.711H.957v2.332A8.997 8.997 0 009 18z" fill="#34A853"></path><path d="M3.964 10.71A5.41 5.41 0 013.682 9c0-.593.102-1.17.282-1.71V4.958H.957A8.996 8.996 0 000 9c0 1.452.348 2.827.957 4.042l3.007-2.332z" fill="#FBBC05"></path><path d="M9 3.58c1.321 0 2.508.454 3.44 1.345l2.582-2.58C13.463.891 11.426 0 9 0A8.997 8.997 0 00.957 4.958L3.964 7.29C4.672 5.163 6.656 3.58 9 3.58z" fill="#EA4335"></path><path d="M0 0h18v18H0z"></path></g></svg></div><span class="_0051d171 _2a3b75a1 f973eed0 f6342c26">Continue with Google</span></a>")
-// }
-// function openAuth() {
-//   const iframe = document.createElement('iframe');
-//   iframe.setAttribute('src', 'https://accounts.google.com/gsi/select?client_id=542417165126-tajqofllkjmbkpjm2uvvg6tskvio897g.apps.googleusercontent.com&ux_mode=popup&ui_mode=card&as=aPZNxhmkBcHKeJpafc1gTw&channel_id=1e2a8848e5f05fb38227cdb6e2e06104ff3cc7f00ca14f9c102ab5071186004f&origin=https%3A%2F%2Fwww.voice-box-ai.com');
-//   iframe.setAttribute('width', 500);
-//   iframe.setAttribute('height', 800);
-
-//   // const container = document.getElementById(containerId);
-//   body.appendChild(iframe);
-// }
-
-// document.getElementById("openAuth").addEventListener("click", () => {
-//   const iframe = document.createElement('iframe');
-//   iframe.setAttribute('src', 'https://accounts.google.com/gsi/select?client_id=542417165126-tajqofllkjmbkpjm2uvvg6tskvio897g.apps.googleusercontent.com&ux_mode=popup&ui_mode=card&as=aPZNxhmkBcHKeJpafc1gTw&channel_id=1e2a8848e5f05fb38227cdb6e2e06104ff3cc7f00ca14f9c102ab5071186004f&origin=https%3A%2F%2Fwww.voice-box-ai.com');
-//   iframe.setAttribute('width', 500);
-//   iframe.setAttribute('height', 800);
-
-
-//   document.getElementById("iframeAuth").appendChild(iframe);
-// })
-
-
-// function onSignIn(googleUser) {
-//   // Get the user's ID token and basic profile information
-//   var id_token = googleUser.getAuthResponse().id_token;
-//   var profile = googleUser.getBasicProfile();
-// console.log(id_token);
-// console.log(profile);
-//   // Send the ID token to your server to authenticate the user
-//   // ...
-// }
-
-
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log(document.domain);//It outputs id of extension to console
@@ -194,62 +161,12 @@ document.addEventListener("DOMContentLoaded", function () {
       }
   });
 });
-// openAuth(500, 800);
-
-// chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-//   if (request.method == "getSelection")
-//     sendResponse({data: window.getSelection().toString()});
-//   else
-//     sendResponse({}); // snub them.
-// });
 
 
 
-
-  // function saveToEmail(translation) {
-  //   var accessToken = null;
-  //   chrome.identity.getAuthToken({interactive: true}, function(token) {
-  //     accessToken = token;
-  //     chrome.identity.getProfileUserInfo(function(userInfo) {
-  //       var email = userInfo.email;
-  //       var subject = "New translation: " + translation;
-  //       var body = "Translation: " + translation;
-  //       var message = "To: " + email + "\r\n" +
-  //                     "Subject: " + subject + "\r\n\r\n" +
-  //                     body;
-  //       var encodedMessage = btoa(message).replace(/\+/g, '-').replace(/\//g, '_');
-  //       var url = "https://www.googleapis.com/gmail/v1/users/" + email + "/messages/send";
-  //       fetch(url, {
-  //         method: "POST",
-  //         headers: {
-  //           "Authorization": "Bearer " + accessToken,
-  //           "Content-Type": "application/json"
-  //         },
-  //         body: JSON.stringify({
-  //           "raw": encodedMessage
-  //         })
-  //       })
-  //       .then(response => response.json())
-  //       .then(data => {
-  //         console.log(data);
-  //       });
-  //     });
-  //   });
-  // }
-
-
-
-  // function saveToEmailAndServer(translation) {
-  //   saveToEmail(translation);
-  //   fetch('https://example.com/api/save-translation', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({translation: translation})
-  //   })
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     console.log(data);
-  //   });
-  // }
+function isSingleWord(str) {
+  // Remove leading and trailing spaces
+  str = str.trim();
+  // Check if there are any spaces left in the string
+  return !/\s/.test(str);
+}
